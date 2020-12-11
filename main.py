7e58717cf22f329627855937397e14d38fecc999
lioncatdev <sankhayan2002@gmail.com>
2020-12-11 20:53:12 +0530

    Organized
    Removed virtual env  because of problems
    Moved build to build folder
    now any bot can be used
    Just add your bot's token into the token part in commands.json

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import discord
 import json
 import os
+
+logfile=open("Serverlog.txt",'a')
 if os.path.exists('commands.json'):
     with open('commands.json') as f:
         data=json.load(f)
         client = discord.Client()
 
 
-        @client.event
-        async def on_message(message):
-            if message.content in data.keys():
-                await message.channel.send(data[message.content])  # If the user says !hello we will send back hi
+    @client.event
+    async def on_message(message):
+        if message.content in data["commands"].keys():
+            await message.channel.send(data["commands"][message.content])
+
+            print(f"message {message.content} written by",message.author,f"at time {message.created_at}")
+            logfile.write(f"message {message.content} written by {message.author} at time {message.created_at} \n")
+
+            print("sent message ",data["commands"][message.content]," to ",message.author)
+            logfile.write("lioncatbot sent message "+data["commands"][message.content]+"\n")
+            logfile.flush()
+    client.run(data["token"])
 
 
-        client.run("Bot token")
 else:
-    print("Error File not found. press any key to exit")
+    print("Error File not found. press Enter to exit")
     e=input()
-
+logfile.close()
